8eeadc15270e55a4f19a4049e22f4f07db9e7985
PCBL <cksgodl1@naver.com>
2022-10-04 03:12:05 +0900

    feat : SearchResultScreen

--- app/src/main/java/com/compose/cocktaildakk_compose/ui/Search/SearchScreen.kt
+++
@@ -1,9 +0,0 @@
-package com.compose.cocktaildakk_compose.ui.Search
-
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-
-@Composable
-fun SearchScreen() {
-  Text(text = "Search Screen")
-}
