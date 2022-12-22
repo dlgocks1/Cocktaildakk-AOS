e91e3a3ef61de5e35dbecf759481fb1ead9ab53d
PCBL <cksgodl1@naver.com>
2022-12-22 19:12:07 +0900

    style : 줄 나누는 함수 추출

---
+++ app/src/main/java/com/compose/cocktaildakk_compose/ui/components/LineSpacer.kt
@@ -0,0 +1,20 @@
+package com.compose.cocktaildakk_compose.ui.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun LineSpacer() {
+    Spacer(
+        modifier = Modifier
+            .height(5.dp)
+            .fillMaxWidth()
+            .background(color = Color(0x40ffffff))
+    )
+}
