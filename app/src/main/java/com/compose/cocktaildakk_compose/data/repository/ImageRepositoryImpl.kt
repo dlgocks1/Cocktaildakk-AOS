6718009a9594bc7be9b02922ae4d49e5307445b5
PCBL <cksgodl1@naver.com>
2022-12-27 00:39:35 +0900

    feat : 갤러리 페이징 구현

--- app/src/main/java/com/compose/cocktaildakk_compose/data/repository/ImageRepositoryImpl.kt
+++ app/src/main/java/com/compose/cocktaildakk_compose/data/repository/ImageRepositoryImpl.kt
@@ -6,62 +6,67 @@ import android.database.Cursor
 import android.net.Uri
 import android.provider.MediaStore
 import android.util.Log
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import com.compose.cocktaildakk_compose.data.data_source.CocktailPagingSource
+import com.compose.cocktaildakk_compose.data.data_source.GalleryPagingSource
+import com.compose.cocktaildakk_compose.domain.model.Cocktail
 import com.compose.cocktaildakk_compose.domain.model.GalleryImage
 import com.compose.cocktaildakk_compose.domain.repository.ImageRepository
 import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class ImageRepositoryImpl @Inject constructor(
     @ApplicationContext private val context: Context,
 ) : ImageRepository {
 
+    private val uriExternal: Uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+    private val projection = arrayOf(
+        MediaStore.Images.ImageColumns.DATA,
+        MediaStore.Images.ImageColumns.DISPLAY_NAME, // 이름
+        MediaStore.Images.ImageColumns.SIZE, // 크기
+        MediaStore.Images.ImageColumns.DATE_TAKEN,
+        MediaStore.Images.ImageColumns.DATE_ADDED, // 추가된 날짜
+        MediaStore.Images.ImageColumns._ID
+    )
+    private val sortedOrder = "${MediaStore.Images.ImageColumns.DATE_ADDED} DESC"
+
     override fun getAllPhotos(
+        page: Int,
         loadSize: Int,
         currentLocation: String?
     ): MutableList<GalleryImage> {
         val galleryImageList = mutableListOf<GalleryImage>()
-        // 외장 메모리에 있는 URI를 받도록 함
-        val uriExternal: Uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
-        // 커서에 가져올 정보에 대해서 지정한다.
-        val query: Cursor?
-        val projection = arrayOf(
-            MediaStore.Images.ImageColumns.DATA,
-            MediaStore.Images.ImageColumns.DISPLAY_NAME, // 이름
-            MediaStore.Images.ImageColumns.SIZE, // 크기
-            MediaStore.Images.ImageColumns.DATE_TAKEN,
-            MediaStore.Images.ImageColumns.DATE_ADDED, // 추가된 날짜
-            MediaStore.Images.ImageColumns._ID
-        )
-        val resolver = context.contentResolver
-
         var selection: String? = null
         var selectionArgs: Array<String>? = null
 
-        query = resolver?.query(
+        val query = context.contentResolver?.query(
             uriExternal,
             projection,
             selection,
             selectionArgs,
-            "${MediaStore.Images.ImageColumns.DATE_ADDED} DESC"
+            sortedOrder
         )
 
         query?.use { cursor ->
-            val idColumn = cursor.getColumnIndexOrThrow(MediaStore.Images.ImageColumns._ID)
-            val nameColumn =
-                cursor.getColumnIndexOrThrow(MediaStore.Images.ImageColumns.DISPLAY_NAME)
-            val filePathColumn = cursor.getColumnIndexOrThrow(MediaStore.Images.ImageColumns.DATA)
-            val sizeColumn = cursor.getColumnIndexOrThrow(MediaStore.Images.ImageColumns.SIZE)
-            val dateColumn = cursor.getColumnIndexOrThrow(MediaStore.Images.ImageColumns.DATE_TAKEN)
-            while (cursor.moveToNext()) {
-                val id = cursor.getLong(idColumn)
-                val filepath = cursor.getString(filePathColumn)
-                val name = cursor.getString(nameColumn)
-                val size = cursor.getInt(sizeColumn)
-                val date = cursor.getString(dateColumn)
+            cursor.move((page - 1) * loadSize)
+            while (cursor.moveToNext() && cursor.position < loadSize * page) {
+                val id =
+                    cursor.getLong(cursor.getColumnIndexOrThrow(MediaStore.Images.ImageColumns._ID))
+                val name =
+                    cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Images.ImageColumns.DISPLAY_NAME))
+                val filepath =
+                    cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Images.ImageColumns.DATA))
+                val size =
+                    cursor.getInt(cursor.getColumnIndexOrThrow(MediaStore.Images.ImageColumns.SIZE))
+                val date =
+                    cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Images.ImageColumns.DATE_TAKEN))
                 val contentUri = ContentUris.withAppendedId(uriExternal, id)
                 galleryImageList.add(
                     GalleryImage(
-                        id,
+                        id = id,
                         filepath = filepath,
                         uri = contentUri,
                         name = name,
@@ -71,8 +76,8 @@ class ImageRepositoryImpl @Inject constructor(
                 )
             }
         }
-
         return galleryImageList
     }
+
 
 }
--- app/src/main/java/com/compose/cocktaildakk_compose/domain/repository/ImageRepository.kt
+++ app/src/main/java/com/compose/cocktaildakk_compose/domain/repository/ImageRepository.kt
@@ -1,7 +1,13 @@
 package com.compose.cocktaildakk_compose.domain.repository
 
+import androidx.paging.PagingData
 import com.compose.cocktaildakk_compose.domain.model.GalleryImage
+import kotlinx.coroutines.flow.Flow
 
 interface ImageRepository {
-    fun getAllPhotos(loadSize: Int = 0, currentLocation: String? = null): MutableList<GalleryImage>
+    fun getAllPhotos(
+        pair: Int,
+        loadSize: Int,
+        currentLocation: String? = null
+    ): MutableList<GalleryImage>
 }
