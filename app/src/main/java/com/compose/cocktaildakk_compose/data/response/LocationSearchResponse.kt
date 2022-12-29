dd36971ee78f1f65faa6ccb665df335956bf804a
PCBL <cksgodl1@naver.com>
2022-12-29 16:18:25 +0900

    feat : LocaionSearch 응답 클래스 추가

---
+++ app/src/main/java/com/compose/cocktaildakk_compose/data/response/LocationSearchResponse.kt
@@ -0,0 +1,14 @@
+package com.compose.cocktaildakk_compose.data.response
+
+
+import com.google.gson.annotations.SerializedName
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class LocationSearchResponse(
+    @SerializedName("documents")
+    val markers: List<Marker>,
+    @SerializedName("meta")
+    val meta: Meta
+)
---
+++ app/src/main/java/com/compose/cocktaildakk_compose/data/response/Meta.kt
@@ -0,0 +1,18 @@
+package com.compose.cocktaildakk_compose.data.response
+
+
+import com.google.gson.annotations.SerializedName
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class Meta(
+    @SerializedName("is_end")
+    val isEnd: Boolean,
+    @SerializedName("pageable_count")
+    val pageableCount: Int,
+    @SerializedName("same_name")
+    val sameName: SameName,
+    @SerializedName("total_count")
+    val totalCount: Int
+)
