0ccdb363acee98779988e6fe5f71663ccfae685c
PCBL <cksgodl1@naver.com>
2022-12-29 16:18:48 +0900

    feat : 네트워크 DI 설정

---
+++ app/src/main/java/com/compose/cocktaildakk_compose/di/NetworkModule.kt
@@ -0,0 +1,99 @@
+package com.compose.cocktaildakk_compose.di
+
+import com.compose.cocktaildakk_compose.BuildConfig
+import com.compose.cocktaildakk_compose.network.MapService
+import com.compose.cocktaildakk_compose.network.ServiceInterceptor
+import com.skydoves.sandwich.adapters.ApiResponseCallAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Qualifier
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    private val httpLoggingInterceptor =
+        HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+
+    @Qualifier
+    @Retention(AnnotationRetention.BINARY)
+    annotation class KakaoInterceptorOkhttpClient
+
+    @Qualifier
+    @Retention(AnnotationRetention.BINARY)
+    annotation class OtherInterceptorOkhttpClinet
+
+    @Qualifier
+    @Retention(AnnotationRetention.BINARY)
+    annotation class KakaoRetrofit
+
+    @Qualifier
+    @Retention(AnnotationRetention.BINARY)
+    annotation class OtherRetrofit
+
+    @KakaoInterceptorOkhttpClient
+    @Provides
+    fun provideKakaoInterceptorOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(httpLoggingInterceptor)
+            .addInterceptor(ServiceInterceptor("KakaoAK ${BuildConfig.KAKAO_API_TOKEN}"))
+            .connectTimeout(20, TimeUnit.SECONDS)
+            .readTimeout(20, TimeUnit.SECONDS)
+            .writeTimeout(20, TimeUnit.SECONDS)
+            .retryOnConnectionFailure(true)
+            .build()
+    }
+
+    @OtherInterceptorOkhttpClinet
+    @Provides
+    fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(httpLoggingInterceptor)
+            .addInterceptor(ServiceInterceptor())
+            .connectTimeout(20, TimeUnit.SECONDS)
+            .readTimeout(20, TimeUnit.SECONDS)
+            .writeTimeout(20, TimeUnit.SECONDS)
+            .retryOnConnectionFailure(true)
+            .build()
+    }
+
+    @Provides
+    @KakaoRetrofit
+    fun provideKaoaoRetrofit(
+        @KakaoInterceptorOkhttpClient okHttpClient: OkHttpClient
+    ): Retrofit {
+        return Retrofit.Builder()
+            .client(okHttpClient)
+            .baseUrl("https://dapi.kakao.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .addConverterFactory(MoshiConverterFactory.create())
+            .addCallAdapterFactory(ApiResponseCallAdapterFactory.create())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideMapService(
+        @KakaoRetrofit retrofit: Retrofit
+    ): MapService {
+        return retrofit.create(MapService::class.java)
+    }
+
+//    @Provides
+//    @Singleton
+//    fun provideUbtClient(
+//        closService: ClosService,
+//    ): ClosClient {
+//        return ClosClient(closService, feedService, cloverService)
+//    }
+
+}
---
+++ app/src/main/java/com/compose/cocktaildakk_compose/network/MapService.kt
@@ -0,0 +1,19 @@
+package com.compose.cocktaildakk_compose.network
+
+import com.compose.cocktaildakk_compose.data.response.LocationSearchResponse
+import com.compose.cocktaildakk_compose.ui.naverMap.NaverMapViewModel.Companion.SEARCH_RADIUS
+import com.skydoves.sandwich.ApiResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface MapService {
+    @GET("/v2/local/search/keyword.json?")
+    suspend fun locationSearch(
+        @Query("query") query: String,
+        @Query("y") lat: Double,
+        @Query("x") lon: Double,
+        @Query("radius") radius: Int = SEARCH_RADIUS
+    ): ApiResponse<LocationSearchResponse>
+
+}
